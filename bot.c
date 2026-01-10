01da9674ec8307a935b0998c0a60a66aa0754ccb
Cuvilliez <fiona.cuvilliez@etu.univ-cotedazur.fr>
2026-01-10 10:17:51 +0100

    jsp

---
+++ bot.c
@@ -0,0 +1,115 @@
+#include "data.h"
+#include "game.h"
+#include "bot.h"
+#include "logger.h"
+#include "evaluate.h"
+
+
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+
+int coups = 0;
+
+
+
+
+
+int get_move_list(Board* board, Move* move_list, int player) { 
+    int internal_player = player % 2;
+    int index = 0;
+    // DEBUG_PRINT("Possible moves : \n"); 
+    // Function to get possible moves
+    for (int i = internal_player ; i < MAX_HOLES; i+=2) { 
+        // Check each hole for possible moves 
+        Hole* hole = get_hole(board, i); 
+        int nb_graines_R = hole->R; 
+        int nb_graines_B = hole->B; 
+        int nb_graines_T = hole->T; 
+        if(nb_graines_B>0) {
+            move_list[index++] = (Move){i, B};
+            // DEBUG_PRINT("Move B, hole %d\n", i ); 
+        }
+        if(nb_graines_R>0) {
+            move_list[index++] = (Move){i, R};
+            // DEBUG_PRINT("Move R, hole %d\n", i ); 
+        }
+        if(nb_graines_T>0) { 
+            move_list[index++] =  (Move){i, TB};
+            // DEBUG_PRINT("Move TB, hole %d\n", i ); 
+            move_list[index++] =  (Move){i, TR};
+            // DEBUG_PRINT("Move TR, hole %d\n", i ); 
+        }
+
+    } 
+    // DEBUG_PRINT("Nb total de moves : %d\n", index);
+    return index;
+}
+
+int estimation_nb_moves(Board* board) {
+    // Estimate the number of possible moves for player
+    int count = 0;
+    count += board->seed_count / 2 * 4; // Rough estimate based on seed count
+    return count;
+}
+
+int compute_depth(Board* board) {
+    int total_seeds = board->seed_count;
+    Move movesL[MAX_HOLES/2 * 4];
+    int moves = estimation_nb_moves(board);
+
+    if (moves > 16) return 4;
+    if (moves > 12) return 5;
+    if (moves > 8)  return 6;
+
+    if (total_seeds > 30) return 4;
+    if (total_seeds > 20) return 8;
+
+    if (total_seeds > 12) return 10;
+    if (total_seeds > 8)  return 12;
+
+    return 14;
+}
+
+void bot_play(Board* board) {
+    coups ++;
+    // Fonction principale du bot qui choisit et joue un coup
+    Move bestMove;
+    struct timeval debut, fin;
+    gettimeofday(&debut, NULL);
+    // exit(0);
+
+    int profondeur = compute_depth(board);;
+    // bestMove = decisionMinMax(board, PLAYER, profondeur);
+    // fprintf(stderr, "---------------------------------------\n");
+    bestMove = decisionAlphaBeta(board, PLAYER, profondeur);
+    // fprintf(stderr, "Hole index: %d, Seed type: %s\n", bestMove.hole_index, 
+    //     (bestMove.type == R) ? "R" : 
+    //     (bestMove.type == B) ? "B" : 
+    //     (bestMove.type == TR) ? "TR" : "TB");
+
+    if (!is_valid_move(board, bestMove.hole_index, bestMove.type, PLAYER)) {
+        fprintf(stderr, "Bot selected an invalid move. Skipping turn.\n"); // Debug message
+        exit(EXIT_FAILURE);
+        return;
+    }
+    // fprintf(stderr, "Bot selected move: hole %d, type %d\n", bestMove.hole_index, bestMove.type); // Debug message
+    // log("Bot selected move: hole %d, type %s", bestMove.hole_index, 
+        // (bestMove.type == R) ? "R" : 
+        // (bestMove.type == B) ? "B" : 
+        // (bestMove.type == TR) ? "TR" : "TB");
+        
+    make_move(board, bestMove.hole_index, bestMove.type, PLAYER);
+
+    printf("%d %s\n", bestMove.hole_index + 1, 
+        (bestMove.type == R) ? "R" : 
+        (bestMove.type == B) ? "B" : 
+        (bestMove.type == TR) ? "TR" : "TB");
+    fflush(stdout);
+
+    gettimeofday(&fin, NULL);
+    double temps_ms = (fin.tv_sec - debut.tv_sec) * 1000.0 +
+                      (fin.tv_usec - debut.tv_usec) / 1000.0;
+    // DEBUG_PRINT("Bot move took %.2f ms\n", temps_ms);
+    fprintf(stderr, "Bot move took %.2f ms\n", temps_ms);
+}
